c98e32e3ef62b5decb7ae980fa74f5ec0986c413
Samuel Piecz <samuel.piecz@gmail.com>
2019-03-20 14:25:44 -0500

    Removed a bunch.

--- TextFieldFrame.java
+++ TextFieldFrame.java
@@ -25,25 +25,9 @@ public class TextFieldFrame extends JFrame
       textField1 = new JTextField(10); 
       add(textField1); // add textField1 to JFrame
 
-      // construct textfield with default text
-      textField2 = new JTextField("Enter text here");
-      add(textField2); // add textField2 to JFrame
-
-      // construct textfield with default text and 21 columns
-      textField3 = new JTextField("Uneditable text field", 21);
-      textField3.setEditable(false); // disable editing
-      add(textField3); // add textField3 to JFrame
-
-      // construct passwordfield with default text
-      passwordField = new JPasswordField("Hidden text");
-      add(passwordField); // add passwordField to JFrame
-
       // register event handlers
       TextFieldHandler handler = new TextFieldHandler();
       textField1.addActionListener(handler);
-      textField2.addActionListener(handler);
-      textField3.addActionListener(handler);
-      passwordField.addActionListener(handler);
    } // end TextFieldFrame constructor
 
    // private inner class for event handling
@@ -57,21 +41,6 @@ public class TextFieldFrame extends JFrame
          // user pressed Enter in JTextField textField1
          if (event.getSource() == textField1)
             string = String.format("textField1: %s",
-               event.getActionCommand());
-
-         // user pressed Enter in JTextField textField2
-         else if (event.getSource() == textField2)
-            string = String.format("textField2: %s",
-               event.getActionCommand());
-
-         // user pressed Enter in JTextField textField3
-         else if (event.getSource() == textField3)
-            string = String.format("textField3: %s", 
-               event.getActionCommand());
-
-         // user pressed Enter in JTextField passwordField
-         else if (event.getSource() == passwordField)
-            string = String.format("passwordField: %s", 
                event.getActionCommand());
 
          // display JTextField content
